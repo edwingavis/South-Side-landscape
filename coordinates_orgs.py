6c95679175f17dfb9dcacb2150bdcf39fe014f3d
edwingavis <egavis@uchicago.edu>
2019-02-05 10:32:12 -0600

    new files

--- coordinates_orgs.py
+++ coordinates_orgs.py
@@ -18,7 +18,7 @@ geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1, max_retries=10,
                       error_wait_seconds=10.0)
 
 def previously_done():
-    with open("pretty_orgs_lat_long.csv") as f:
+    with open("orgs_lat_long.csv") as f:
         spamreader = csv.reader(f)
         next(spamreader)
         rv = []
@@ -47,9 +47,9 @@ def fix_street(street):
 def main():
     bar = progressbar.ProgressBar(max_value=19807)
     done = previously_done()
-    with open("pretty_orgs_lat_long_2.csv", "w") as f1:
+    with open("orgs_lat_long_2.csv", "w") as f1:
         spamwriter = csv.writer(f1)
-        with open("pretty_orgs_addresses.csv", "r") as f2:  
+        with open("orgs_addresses.csv", "r") as f2:  
             org_reader = csv.reader(f2)
             spamwriter.writerow(next(org_reader) + ["lat", "lon"])
             c = 0
